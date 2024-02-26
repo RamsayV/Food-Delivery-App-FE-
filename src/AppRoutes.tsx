25b24b1be16b3c95f166df6f93c773af54d274b7
Ramsay <ramsayveitch123@gmail.com>
2024-02-26 21:31:09 +0000

    added filtering and restaurant searches

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -5,6 +5,8 @@ import AuthCallBackPage from "./pages/AuthCallBackPage";
 import UserProfilePage from "./pages/UserProfilePage";
 import ProtectedRoute from "./auth/ProtectedRoute";
 import ManageRestaurantPage from "./pages/ManageRestaurantPage";
+import SearchPage from "./pages/SearchPage";
+import { useEffect } from "react";
 
 const AppRoutes = () => {
   return (
@@ -18,6 +20,7 @@ const AppRoutes = () => {
           }
         />
       <Route path="auth-callback" element={<AuthCallBackPage />} />
+      <Route path="/search/:city" element={<Layout showHero={false}><SearchPage /></Layout>} />
       <Route element={<ProtectedRoute />}>
       <Route
         path="/user-profile"
@@ -42,3 +45,4 @@ const AppRoutes = () => {
 };
 
 export default AppRoutes;
+
